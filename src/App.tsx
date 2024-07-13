f27e5af27606f942f35732ebc6c5a53f0dbe54f5
ASR-01 <gamingasr002@gmail.com>
2024-07-13 23:03:15 +0530

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Sales = React.lazy(() => import("./pages/Sales"));
+const About = React.lazy(() => import("./pages/About"));
 
 const App = () => {
   return (
-    <div>
-      <h1>Jai Bhole</h1>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/sales" element={<Sales />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
