b8b7cee071edc39af143c10486874a43f643089d
ASR-01 <gamingasr002@gmail.com>
2024-07-14 16:54:03 +0530

    contact_05

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
-
+import "./Home.css";
 const Home = () => {
   return (
-     <section className="container section section__height home_component" id="home">
-     <h2 className="section__title">Home</h2>
-   </section>
-  )
-}
+    <section
+      className="container section section__height home_component"
+      id="home"
+    >
+      <h2 className="section__title">Home</h2>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
