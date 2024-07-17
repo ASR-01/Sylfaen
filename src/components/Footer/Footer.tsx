f55fefb2a5f3492bfc7c8a740736d217bd1e4e84
ASR-01 <gamingasr002@gmail.com>
2024-07-17 08:32:54 +0530

    addes address

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -43,7 +43,7 @@ const Footer = () => {
             <h4>Find us here</h4>
             <iframe
               className="footer-col_iframe"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.2886858878483!2d72.8761457747253!3d19.269808145860612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0616319475f%3A0x4ab0e1213c0c75c2!2sAmar%20Palace!5e0!3m2!1sen!2sin!4v1720951832104!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.1863052734107!2d72.84412937753348!3d19.23071060072491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0d70da75639%3A0x2eafc54ea49fe390!2sChandavarkar%20Rd%2C%20Borivali%2C%20Mumbai%2C%20Maharashtra%20400092!5e0!3m2!1sen!2sin!4v1721184702197!5m2!1sen!2sin"
               width="600"
               height="450"
               loading="lazy"
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
 
         <div className="contact_container_content_addresses">
           <div className="contact_container_content_addresses_01">
-            <h3>Registration Office</h3>
+            <h3>Registration </h3>
             <h4>Address:</h4>
             <p>
               Mira Co-op Housing Society Building No A3, 02 Behind Hotel Amar
@@ -23,9 +23,12 @@ const Contact = () => {
           </div>
 
           <div className="contact_container_content_addresses_01 office_zone">
-            <h3>Office Address</h3>
+            <h3>Office</h3>
             <h4>Address:</h4>
-            <p> F 96, The Zone  Chandavarkar Road, Borivali West Mumbai 400092</p>
+            <p>
+              {" "}
+              F 96, The Zone Chandavarkar Road, Borivali West Mumbai 400092
+            </p>
           </div>
         </div>
 
@@ -67,6 +70,7 @@ const Contact = () => {
           <div>
             <h4>Phone:</h4>
             <ul>
+            <li style={{fontWeight:"700"}}>Adv Yogesh S Jagdale</li>
               <li>Registration Office: +91 9323572423</li>
               <li>Borivali Office: +91 7045932108</li>
             </ul>
@@ -83,7 +87,7 @@ const Contact = () => {
 
         <div className="location">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.2886858878483!2d72.8761457747253!3d19.269808145860612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0616319475f%3A0x4ab0e1213c0c75c2!2sAmar%20Palace!5e0!3m2!1sen!2sin!4v1720951832104!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.1863052734107!2d72.84412937753348!3d19.23071060072491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b0d70da75639%3A0x2eafc54ea49fe390!2sChandavarkar%20Rd%2C%20Borivali%2C%20Mumbai%2C%20Maharashtra%20400092!5e0!3m2!1sen!2sin!4v1721184702197!5m2!1sen!2sin"
             width="600"
             height="450"
             loading="lazy"
